c391d3d29716b901b2b364a398d8856e9432d8b2
Ravish <ravishxt@gmail.com>
2021-11-12 20:24:25 +0530

    Home page done

--- views/static/js/home.js
+++ views/static/js/home.js
@@ -1,37 +1,5 @@
- // //Movement Animation to happen
- // const card = document.querySelector('.card');
- // const container = document.querySelector('.container');
- // //Items
- // const title = document.querySelector(".title");
- // const description = document.querySelector(".info h3");
-
- // //Moving Animation Event
- // container.addEventListener("mousemove", (e) => {
- //         let xAxis = (window.innerWidth / 2 - e.pageX) / 50;
- //         let yAxis = (window.innerWidth / 2 - e.pageY) / 50;
- //         card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
- //         //  card.style.classList.add('rotate');
- //     })
- //     //Animate In
- // container.addEventListener("mouseenter", (e) => {
- //     card.style.transition = "none";
- //     //Popout
- //     title.style.transform = "translateZ(120px)";
- //     description.style.transform = "translateZ(110px)";
- //     //card.style.transform= `rotateY(${180}deg)`;
- //     // card.style.transition = "all 0.5s ease";
- // });
- // //Animate Out
- // container.addEventListener("mouseleave", (e) => {
- //     card.style.transform = `rotateY(0deg) rotateX(0deg)`;
- //     card.style.transition = "all 0.5s ease";
- //     title.style.transform = "translateZ(0px)";
- //     description.style.transform = "translateZ(0px)";
- // })
-
-
 //Movement Animation to happen
-const card = document.querySelector('.card-jee-neet')
+const card = document.querySelectorAll('.card-jee-neet')
 const container = document.querySelector('.jee-neet')
 //Items
 const title = document.querySelector('.title-card-jee-neet h1')
@@ -41,22 +9,26 @@ const description = document.querySelector('.info-card-jee-neet h3')
 container.addEventListener('mousemove', (e) => {
   let xAxis = (window.innerWidth / 2 - e.pageX) / 40
   let yAxis = (window.innerWidth / 2 - e.pageY) / 40
-  card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`
-  //  card.style.classList.add('rotate');
+  card.forEach(cardElement => {
+    cardElement.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`
+  });
 })
 //Animate In
 container.addEventListener('mouseenter', (e) => {
-  card.style.transition = 'none'
   //Popout
   title.style.transform = 'translateZ(90px)'
   description.style.transform = 'translateZ(110px)'
-  card.style.transform = `rotateY(${180}deg)`
-  card.style.transition = 'all 0.5s ease'
+  card.forEach(cardElement => {
+    cardElement.style.transition = 'none'
+    cardElement.style.transition = 'all 0.5s ease'
+  });
 })
 //Animate Out
 container.addEventListener('mouseleave', (e) => {
-  card.style.transform = `rotateY(0deg) rotateX(0deg)`
-  card.style.transition = 'all 0.5s ease'
+  card.forEach(cardElement => {
+  cardElement.style.transform = `rotateY(0deg) rotateX(0deg)`
+  cardElement.style.transition = 'all 0.5s ease'
+  });
   title.style.transform = 'translateZ(0px)'
   description.style.transform = 'translateZ(0px)'
 })
