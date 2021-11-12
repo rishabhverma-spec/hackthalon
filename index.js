b49ed476df95a29ee4c2ae4713eb7d5d33971ea2
rishabhverma-spec <rishabhverma132001@gmail.com>
2021-11-12 22:44:07 +0530

    aim-page

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-var express = require('express');
-var app = express();
-app.set('view engine', 'ejs');
-app.set('views','./views');
-app.use(express.static("views/static"));
-app.get('/', function(req, res){
-   res.render("home");
-});
-console.log("Server Started on port 8080");
-app.listen(8080);
+var express = require('express')
+var app = express()
+app.set('view engine', 'ejs')
+app.set('views', './views')
+app.use(express.static('views/static'))
+app.get('/', function (req, res) {
+  res.render('home')
+})
+app.get('/aim', function (req, res) {
+  res.render('aim')
+})
+console.log('Server Started on port 8080')
+app.listen(8080)
