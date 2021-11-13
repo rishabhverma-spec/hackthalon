58cbdf1582453a875bc92d90acd5ef9b97e82df5
rishabhverma-spec <rishabhverma132001@gmail.com>
2021-11-13 18:26:42 +0530

    contact page

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-var express = require('express');
-var app = express();
-app.set('view engine', 'ejs');
-app.set('views','./views');
-app.use(express.static("views/static"));
-app.get('/', function(req, res){
-   res.render("home");
-});
-app.get('/iit', function(req, res){
-   res.render("iit");
-});
-app.get('/neet', function(req, res){
-   res.render("neet");
-});
-app.get('/aim', function(req, res){
-   res.render("aim");
-});
-console.log("Server Started on port 8080");
-app.listen(8080);
+var express = require('express')
+var app = express()
+app.set('view engine', 'ejs')
+app.set('views', './views')
+app.use(express.static('views/static'))
+app.get('/', function (req, res) {
+  res.render('home')
+})
+app.get('/iit', function (req, res) {
+  res.render('iit')
+})
+app.get('/neet', function (req, res) {
+  res.render('neet')
+})
+app.get('/aim', function (req, res) {
+  res.render('aim')
+})
+app.get('/contact', function (req, res) {
+  res.render('contact')
+})
+console.log('Server Started on port 8080')
+app.listen(8080)
