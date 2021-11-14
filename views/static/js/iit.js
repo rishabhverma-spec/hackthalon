355eb362e06eaa1dc60c55b6a8a8adb58148d51c
rishabhverma-spec <rishabhverma132001@gmail.com>
2021-11-14 10:18:08 +0530

    links

--- views/static/js/iit.js
+++ views/static/js/iit.js
@@ -5,7 +5,7 @@
     x.style.display="none";
       if(state=="All India"){
         x.style.display="inline";
-        document.getElementById("one").innerHTML="Jossa";
+        document.getElementById("one").innerHTML="JOSSA";
         document.getElementById("two").innerHTML="CSAB";      
     }
     else if(state=="Delhi"){
@@ -20,43 +20,43 @@
         var counselling = document.getElementById("counselling").value;
         var dynamic = document.getElementById("dynamic");
          if(state=="Himachal Pradesh"){
-            dynamic.innerHTML = "<a href=\"https://www.himtu.ac.in/admission/\" target=\"blank\">" + "himtu.ac.in/admission/" + "</a>";      
+            dynamic.innerHTML = "<a href=\"https://www.himtu.ac.in/admission/\" target=\"_blank\">" + "himtu.ac.in/admission/" + "</a>";      
          }
          else if(state=="Haryana"){
-            dynamic.innerHTML = "<a href=\"http://hstes.org.in/\" target=\"blank\">" + "hstes.org.in" + "</a>";  
+            dynamic.innerHTML = "<a href=\"http://hstes.org.in/\" target=\"_blank\">" + "hstes.org.in" + "</a>";  
          }
-         else if(state=="Gujrat"){
-            dynamic.innerHTML = "<a href=\"https://gujacpc.nic.in/RegSys/Page/Page?PageId=1&LangId=P\" target=\"blank\">" + "gujacpc.nic.in" + "</a>";  
+         else if(state=="Gujarat"){
+            dynamic.innerHTML = "<a href=\"https://gujacpc.nic.in/RegSys/Page/Page?PageId=1&LangId=P\" target=\"_blank\">" + "gujacpc.nic.in" + "</a>";  
          }
          else if(state=="Uttar Pradesh"){
-            dynamic.innerHTML = "<a href=\"https://jeecup.nic.in/WebInfo/Page/Page?PageId=1&LangId=P\" target=\"blank\">" + "jeecup.nic.in" + "</a>";  
+            dynamic.innerHTML = "<a href=\"https://jeecup.nic.in/WebInfo/Page/Page?PageId=1&LangId=P\" target=\"_blank\">" + "jeecup.nic.in" + "</a>";  
 
          }
          else if(state=="West Bengal"){
-            dynamic.innerHTML = "<a href=\"https://wbjeeb.nic.in/WBJEEBBoardCMS/Page/Page?PageId=1&LangId=P\" target=\"blank\">" + "wbjeeb.nic.in" + "</a>"; 
+            dynamic.innerHTML = "<a href=\"https://wbjeeb.nic.in/WBJEEBBoardCMS/Page/Page?PageId=1&LangId=P\" target=\"_blank\">" + "wbjeeb.nic.in" + "</a>"; 
          }
          else if(state=="Rajasthan"){
-         dynamic.innerHTML = "<a href=\"https://reap2021.com/\" target=\"blank\">" + "reap2021.com" + "</a>";
+         dynamic.innerHTML = "<a href=\"https://reap2021.com/\" target=\"_blank\">" + "reap2021.com" + "</a>";
          }
          else if(state=="Punjab"){
-            dynamic.innerHTML = "<a href=\"https://ptu.ac.in/admissions\" target=\"blank\">" + "ptu.ac.in/admissions" + "</a>";  
+            dynamic.innerHTML = "<a href=\"https://ptu.ac.in/admissions\" target=\"_blank\">" + "ptu.ac.in/admissions" + "</a>";  
          }
          else if(state=="Chandigarh")
          {
-           dynamic.innerHTML = "<a href=\"https://chdenggadmissions.nic.in/WebInfo/Page/Page?PageId=1&LangId=P\" target=\"blank\">" + "chdenggadmissions.nic.in" + "</a>";
+           dynamic.innerHTML = "<a href=\"https://chdenggadmissions.nic.in/WebInfo/Page/Page?PageId=1&LangId=P\" target=\"_blank\">" + "chdenggadmissions.nic.in" + "</a>";
          }
          else if((state=="Delhi")&&(counselling=="JAC-Delhi")){
-            dynamic.innerHTML = "<a href=\"https://jacdelhi.nic.in/WebInfo/Page/Page?PageId=1&LangId=P\" target=\"blank\">" + "jacdelhi.nic.in" + "</a>";
+            dynamic.innerHTML = "<a href=\"https://jacdelhi.nic.in/WebInfo/Page/Page?PageId=1&LangId=P\" target=\"_blank\">" + "jacdelhi.nic.in" + "</a>";
          }
          else if((state=="Delhi")&&(counselling=="I.P.U")){
-         dynamic.innerHTML = "<a href=\"https://ipu.admissions.nic.in/IpuAdmiss/page/Page?PageId=1&LangId=P\" target=\"blank\">" + "ipu.admissions.nic.in" + "</a>";  
+         dynamic.innerHTML = "<a href=\"https://ipu.admissions.nic.in/IpuAdmiss/page/Page?PageId=1&LangId=P\" target=\"_blank\">" + "ipu.admissions.nic.in" + "</a>";  
         }
-        else if((state=="All India")&&(counselling=="Jossa")){
-            dynamic.innerHTML = "<a href=\"https://josaa.nic.in/WebInfo/Page/Page?PageId=1&LangId=P\" target=\"blank\">" + "josaa.nic.in" + "</a>";
+        else if((state=="All India")&&(counselling=="JOSSA")){
+            dynamic.innerHTML = "<a href=\"https://josaa.nic.in/WebInfo/Page/Page?PageId=1&LangId=P\" target=\"_blank\">" + "josaa.nic.in" + "</a>";
         }
         else if((state="All India")&&(counselling="CSAB"))
         {
-            dynamic.innerHTML = "<a href=\"https://csab.nic.in/webinfo/Page/Page?PageId=1&LangId=P\" target=\"blank\">" + "csab.nic.in" + "</a>";
+            dynamic.innerHTML = "<a href=\"https://csab.nic.in/webinfo/Page/Page?PageId=1&LangId=P\" target=\"_blank\">" + "csab.nic.in" + "</a>";
         }
 
     }
